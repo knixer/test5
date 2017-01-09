e5752e2919762b282829399dbfaa0aa65656c849
Christopher <christopher.ono@gmail.com>
2017-01-09 11:46:31 +0100

    Onodig andring
    onodig andring

--- Note.java
+++ Note.java
@@ -50,7 +50,7 @@ public class Note {
 
     public String toString(){
         return "ID: " + noteId + "Title: " + title + "Message: " + message + " IconID: " + category.name()
-                + " Date: ";
+                + " Date: " + " TEST";
     }
 
     public int getAssociatedDrawable(){
